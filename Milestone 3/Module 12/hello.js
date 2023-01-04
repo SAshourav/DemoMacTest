ddfc178cfb420047f66f9d3ca597f1c4eb1ae550
Sabbir Ahmed <sabbirahmed2013303@gmail.com>
2023-01-05 03:21:06 +0600

    new work

---
+++ Milestone 3/Module 12/hello.js
@@ -0,0 +1,17 @@
+//variable and variable type
+
+var BananaPrice = 12;
+console.log(typeof BananaPrice);
+var seenAfter = 21;
+console.log(typeof seenAfter);
+
+var name = "Sabbir Ahmed";
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+
+console.log(typeof isHot);
+
+//12.4 Variable Naming and best practices
+
