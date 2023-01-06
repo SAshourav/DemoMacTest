906f8034bd377dfe5f6cf82b725126eaaa8ebfa8
Sabbir Ahmed <sabbirahmed2013303@gmail.com>
2023-01-07 01:40:09 +0600

    event start

---
+++ MileStone 5/module 25 - event/js/event.js
@@ -0,0 +1,15 @@
+function colorChange(){
+    document.body.style.backgroundColor = "yellow";
+}
+
+//GETTING THE ELEMENT FROM THE HTML USING ID
+
+const BlueBtn = document.getElementById("blue");
+BlueBtn.onclick =colorChange2; //just set the name of the function
+function colorChange2(){
+    document.body.style.backgroundColor = "blue";
+}
+const dfltBtn = document.getElementById("dflt");
+dfltBtn.onclick = function defaultColor(){ //এভাবে কাজ করলে , ফাংশন এর নাম না দিলেও হয় 
+    document.body.style.backgroundColor = "white";
+}
